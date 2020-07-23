37ceab0d921b460a144225d24a394e6be1d4d769
Leandro-Suero <ing.leandrosuero@gmail.com>
2020-07-22 23:23:11 -0300

    added jobs from API and pagination

--- src/App.js
+++ src/App.js
@@ -1,10 +1,44 @@
-import React from "react";
-import { Alert } from "react-bootstrap";
+import React, { useState } from "react";
+import { Container } from "react-bootstrap";
+
+import useFetchJobs from "./useFetchJobs";
+import Job from "./components/Job";
+import JobsPagination from "./components/JobsPagination";
+
 function App() {
+    const [params, setParams] = useState({});
+    const [page, setPage] = useState(1);
+    const { jobs, loading, error, hasNextPage } = useFetchJobs(params, page);
+
+    function handleParamChange(e) {
+        const param = e.target.name;
+        const value = e.target.value;
+        console.log(param, value);
+        setPage(1);
+        setParams((prevParams) => {
+            return { ...prevParams, [param]: value };
+        });
+    }
+
     return (
-        <div className="App">
-            <Alert variant="primary">Hello world</Alert>
-        </div>
+        <Container className="my-4">
+            <h1 className="mb-4">GitHub Jobs</h1>
+            <JobsPagination
+                page={page}
+                setPage={setPage}
+                hasNextPage={hasNextPage}
+            />
+            {loading && <h1>Loading...</h1>}
+            {error && <h1>Error. Try refreshing.</h1>}
+            {jobs.map((job) => {
+                return <Job key={job.id} job={job} />;
+            })}
+            <JobsPagination
+                page={page}
+                setPage={setPage}
+                hasNextPage={hasNextPage}
+            />
+        </Container>
     );
 }
 
